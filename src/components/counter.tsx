d876aa1a6f56e6fa658f479824afa9b42b3106b5
eyalgromzin <eyalgromzin@gmail.com>
2023-12-20 16:52:44 +0200

    asd

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -10,12 +10,34 @@ const Counter: FC<dummyProps> = () => {
     const dispatch = useDispatch();
     const currentAge = useSelector((state: RootState) => state.person.age);
 
+    function sendGetRequest(): void {
+        fetch('http://localhost:3000/test/test')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error(`Fetch error: ${error.message}`);
+        });
+    }
+
     return (
         <div>
             <div>count: {currentAge}</div>
             <button onClick={() => {
                  dispatch(setAge(currentAge + 1))
             }}> inc count</button>
+
+            <div>
+                <button onClick={() => sendGetRequest()}>
+                    sendApiRequest
+                </button>
+            </div>
         </div>
     );
 };
